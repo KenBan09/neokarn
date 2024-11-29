fdb508091b40ec91244de120187dbfc8a808e26a
KenBan09 <keeno.pkt@gmail.com>
2024-11-29 15:50:08 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,30 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main() {
+    int num ;
+    int n=1;
+    int even = 0;
+    int odd = 0;
+
+    while (n != 0) {
+        cout << "Enter an integer: ";
+        cin >> num;
+
+        if (num == 0) {
+              break;
+        }
+        if (num % 2 == 0) {
+            even++;
+        } 
+        else {
+            odd++;
+        }
+      
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
+
     return 0;
 }
